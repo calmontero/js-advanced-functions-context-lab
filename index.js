24db6c6fbe30cce8e7bd258e481b4958ecd5a2ca
Carlos <carlos.alfonso.montero@gmail.com>
2021-05-05 21:28:34 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -9,6 +9,67 @@
  for you to use if you need it!
  */
 
+function createEmployeeRecord(employeeObj) {
+    let employeeRecord = {
+        firstName: employeeObj[0],
+        familyName: employeeObj[1],
+        title: employeeObj[2],
+        payPerHour: employeeObj[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employeeRecord;
+}
+
+function createEmployeeRecords(arrayRecord) {
+    let newRecord = arrayRecord.map(function(arrayEmployee){
+      return createEmployeeRecord(arrayEmployee);
+    })
+    return newRecord;
+}
+
+function createTimeInEvent(dateStamp){
+    let [date, hour] = dateStamp.split(' ');
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this;
+}
+
+function createTimeOutEvent(dateStamp){
+    let [date, hour] = dateStamp.split(' ');
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this;
+}
+
+function hoursWorkedOnDate(dateStamp) {
+    let dayIn = this.timeInEvents.find(function(e){
+        return e.date === dateStamp
+    })
+
+    let dayOut = this.timeOutEvents.find(function(e){
+        return e.date === dateStamp
+    })
+
+    let hoursWorked = (dayOut.hour - dayIn.hour) / 100;
+    return hoursWorked;
+}
+
+function wagesEarnedOnDate(dateStamp) {
+    let payOwed = this.payPerHour * hoursWorkedOnDate.call(this,dateStamp);
+    return parseFloat(payOwed.toString());
+}
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -19,4 +80,15 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function findEmployeeByFirstName(srcArray,firstName) {
+    return srcArray.find(function(e) {
+        return e.firstName === firstName;
+    })
+}
+
+function calculatePayroll(employeeObj) {
+    let payRoll = employeeObj.reduce((count, pay) => count + allWagesFor.call(pay), 0);
+    return payRoll;
 }
